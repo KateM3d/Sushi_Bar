ba3c83594c46d5b6bbdc8df3fe750567451309c7
KateM3d <ekaterina8medvedeva@gmail.com>
2021-07-17 20:05:15 -0400

    added styles

--- script.js
+++ script.js
@@ -45,3 +45,18 @@ function newHeader() {
     heading.textContent = "We are open! ";
     heading.classList.add("red");
 }
+
+const items = document.querySelectorAll(".item");
+items.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        removeFocus();
+        item.classList.add("selected");
+    })
+
+
+    removeFocus = () => {
+        items.forEach(item => {
+            item.classList.remove("selected");
+        })
+    }
+})
